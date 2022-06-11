f877b15fe1078128ea28ee84a16271a5700cc841
CagriSakar <cagri.sakar@hotmail.com>
2022-06-11 04:42:47 +0300

    dtolar eklendi.

---
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Data.Models;
+using NLayer.Service;
+
+namespace NLayer.API.Controllers
+{
+    public class CategoriesController : ControllerBase
+    {
+        private readonly CategoryService _categoryService;
+        public CategoriesController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAll();
+            return new ObjectResult(response) { StatusCode = response.Status };
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            return await Get(Id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Category category)
+        {
+            return await Save(category);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Category category)
+        {
+            return await Update(category);
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            return await Delete(productId);
+        }
+    }
+}
---
+++ NLayer.API/Controllers/ProductFeaturesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Data.Models;
+using NLayer.Service;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductFeaturesController :ControllerBase
+    {
+        private readonly ProductFeatureService _productFeatureService;
+
+        public ProductFeaturesController(ProductFeatureService productFeatureService)
+        {
+            _productFeatureService = productFeatureService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _productFeatureService.GetAll();
+            return new ObjectResult(response) { StatusCode = response.Status };
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get(int productId)
+        {
+            return await Get(productId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductFeature product)
+        {
+            return await Save(product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductFeature product)
+        {
+            return await Update(product);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            return await Delete(productId);
+        }
+    }
+}
